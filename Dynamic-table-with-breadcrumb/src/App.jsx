e487fd0c30b8c53c19d415ff1fdcc91aabca8eff
Katarzyna Skibowska <kat.skibowska@gmail.com>
2023-06-24 17:54:52 +0200

    Initial commit

---
+++ Dynamic-table-with-breadcrumb/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Table from "./components/Table";
+
+function App() {
+  return (
+    <>
+      <h2>Books world</h2>
+      <Table />
+    </>
+  );
+}
+
+export default App;
